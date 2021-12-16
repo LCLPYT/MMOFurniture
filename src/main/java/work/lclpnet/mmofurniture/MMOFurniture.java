f3f698706c1dc1e8f0490bc8586977592e550589
LCLP <lukas@lclpnet.work>
2021-12-16 14:29:29 +0100

    init 2

---
+++ src/main/java/work/lclpnet/mmofurniture/MMOFurniture.java
@@ -0,0 +1,11 @@
+package work.lclpnet.mmofurniture;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MMOFurniture implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
