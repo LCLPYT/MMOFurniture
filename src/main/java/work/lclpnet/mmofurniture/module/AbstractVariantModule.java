ddee983f46a6d1609803d21fdf6a685da4237e0b
LCLP <lukas@lclpnet.work>
2021-12-23 18:43:19 +0100

    include missing AbstractVariantModule class

--- src/main/java/work/lclpnet/mmofurniture/module/AbstractVariantModule.java
+++ src/main/java/work/lclpnet/mmofurniture/module/AbstractVariantModule.java
@@ -9,24 +9,31 @@ import work.lclpnet.mmofurniture.util.CommonUtil;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 public abstract class AbstractVariantModule implements IModule {
 
     public static final ImmutableList<String> VARIANTS;
 
-    protected final boolean registerStone;
+    protected final boolean registerStone, registerNetherWood, registerStrippedWood;
 
     public AbstractVariantModule() {
         this(true);
     }
 
     public AbstractVariantModule(boolean registerStone) {
+        this(registerStone, true, true);
+    }
+
+    public AbstractVariantModule(boolean registerStone, boolean registerNetherWood, boolean registerStrippedWood) {
         this.registerStone = registerStone;
+        this.registerNetherWood = registerNetherWood;
+        this.registerStrippedWood = registerStrippedWood;
     }
 
     static {
         List<String> tmp = new ArrayList<>();
-        CommonUtil.WOOD_TYPES.forEach(woodType -> {
+        CommonUtil.OVERWORLD_WOOD_TYPES.forEach(woodType -> {
             tmp.add(woodType);
             tmp.add(String.format("stripped_%s", woodType));
         });
@@ -38,14 +45,16 @@ public abstract class AbstractVariantModule implements IModule {
 
     @Override
     public void register() {
-        CommonUtil.WOOD_TYPES.forEach(woodType -> {
+        Consumer<String> woodRegistrar = woodType -> {
             Block parent = Registry.BLOCK.get(new Identifier(getWoodParentPath(woodType)));
             if (Blocks.AIR.equals(parent)) return;
 
             registerVariant(woodType, parent);
-            registerVariant(String.format("stripped_%s", woodType), parent);
-        });
+            if (registerStrippedWood) registerVariant(String.format("stripped_%s", woodType), parent);
+        };
 
+        CommonUtil.OVERWORLD_WOOD_TYPES.forEach(woodRegistrar);
+        if (registerNetherWood) CommonUtil.NETHER_WOOD_TYPES.forEach(woodRegistrar);
         if (registerStone) {
             CommonUtil.STONE_TYPES.forEach(stoneType -> {
                 Block parent = Registry.BLOCK.get(new Identifier(stoneType));
