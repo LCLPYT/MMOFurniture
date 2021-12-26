ca04f21e0ddd351b7809fba005279d58872c1537
LCLP <lukas@lclpnet.work>
2021-12-26 14:51:27 +0100

    fixed upgraded fence gate recipes

--- src/main/java/work/lclpnet/mmofurniture/module/UpgradedGateModule.java
+++ src/main/java/work/lclpnet/mmofurniture/module/UpgradedGateModule.java
@@ -2,6 +2,7 @@ package work.lclpnet.mmofurniture.module;
 
 import net.fabricmc.fabric.api.tag.TagRegistry;
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraft.tag.Tag;
 import work.lclpnet.mmocontent.block.MMOBlockRegistrar;
 import work.lclpnet.mmofurniture.MMOFurniture;
@@ -9,6 +10,7 @@ import work.lclpnet.mmofurniture.block.UpgradedGateBlock;
 
 public class UpgradedGateModule extends AbstractVariantModule {
 
+    public static Tag<Item> vanillaFenceGates;
     public static Tag<Block> tag;
 
     public UpgradedGateModule() {
@@ -18,6 +20,7 @@ public class UpgradedGateModule extends AbstractVariantModule {
     @Override
     public void register() {
         super.register();
+        vanillaFenceGates = TagRegistry.item(MMOFurniture.identifier("fence_gates"));
         tag = TagRegistry.block(MMOFurniture.identifier("fence_gates/upgraded"));
     }
 
